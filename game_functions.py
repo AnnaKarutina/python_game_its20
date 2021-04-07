bd785b7009523b067c59781d1e0a811f849d4adc
Anna Karutina <anna.karutina@khk.ee>
2021-04-07 22:17:47 +0300

    Limited the bullets number

--- game_functions.py
+++ game_functions.py
@@ -9,8 +9,9 @@ def check_keydown_events(event, game_settings, screen, ship, bullets):
     if event.key == pygame.K_LEFT:
         ship.moving_left = True
     if event.key == pygame.K_SPACE:
-        new_bullet = Bullet(game_settings, screen, ship)
-        bullets.add(new_bullet)
+        if len(bullets) < game_settings.bullets_allowed:
+            new_bullet = Bullet(game_settings, screen, ship)
+            bullets.add(new_bullet)
 
 def check_keyup_events(event, ship):
     """Check key up events"""
