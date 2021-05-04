ee572f0bc65d3f2afcf11f163abc7c1e572097ac
Anna Karutina <anna.karutina@khk.ee>
2021-05-04 09:20:56 +0300

    Refactor bullet and alien collision checking

--- game_functions.py
+++ game_functions.py
@@ -52,6 +52,9 @@ def update_bullets(game_settings, screen, ship, aliens, bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+    check_bullet_alien_collisions(game_settings, screen, ship, aliens, bullets)
+
+def check_bullet_alien_collisions(game_settings, screen, ship, aliens, bullets):
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
     # Remove bullets and create new fleet
     if len(aliens) == 0:
