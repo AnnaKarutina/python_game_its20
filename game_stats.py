338a494eec045fdd44ee212af6ed83745f6c0bb3
Anna Karutina <anna.karutina@khk.ee>
2021-05-10 15:55:53 +0300

    Added scoreboard description

--- game_stats.py
+++ game_stats.py
@@ -9,3 +9,4 @@ class GameStats():
     def reset_stats(self):
         """Initialize statistics, change during the game"""
         self.ships_left = self.game_settings.ship_limit
+        self.score = 0
